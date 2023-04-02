4e74fec30037df24fbfc7290256aededcd1c689b
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2023-04-02 18:11:45 +0200

    image to pdf with size as argument

--- generate.py
+++ generate.py
@@ -2,6 +2,8 @@ import os
 
 import qrcode
 from PIL import Image
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 from config import OUTPUT_FOLDER
 
@@ -19,4 +21,37 @@ def generate_qrcode(
     qr.make(fit=True)
     qr_img = qr.make_image(fill_color=pixel_color, back_color=background_color)
 
-    qr_img.save(os.path.join(OUTPUT_FOLDER, output_file))
+    output_file = os.path.join(OUTPUT_FOLDER, output_file)
+    qr_img.save(output_file)
+    return output_file
+
+
+def image_to_pdf(image_path: str, size: float, title: str) -> None:
+    """Converts an image to a PDF file
+    - image_path: path to the image to convert
+    - size: size of the image in cm
+    - title: title of the PDF file
+    """
+    image = Image.open(image_path)
+    width, height = image.size
+
+    # convert cm to points (1 cm = 28.35 points)
+    size = size * 28.35
+
+    # calculate the position and size of the image in the PDF
+    x = (letter[0] - size) / 2
+    y = (letter[1] - size) / 2
+    aspect_ratio = width / float(height)
+    if aspect_ratio > 1:
+        pdf_width = size
+        pdf_height = size / aspect_ratio
+    else:
+        pdf_width = size * aspect_ratio
+        pdf_height = size
+
+    # create new PDF canvas and embed the image in it
+    pdf_file = f"{image_path.split('.')[0]}.pdf"
+    c = canvas.Canvas(pdf_file, pagesize=letter)
+    c.setTitle(title)
+    c.drawImage(image_path, x, y, width=pdf_width, height=pdf_height)
+    c.save()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from generate import generate_qrcode
+from generate import generate_qrcode, image_to_pdf
 
 if __name__ == "__main__":
-    generate_qrcode(
+    qrcode_image = generate_qrcode(
         url="https://www.notion.so/alexandre-girbal/Le-Clos-des-Mimosas-62f8e60f647348e88c8d1973d8bffbf9",
         pixel_color="#000000",
         background_color="#ffffff",
         size=100,
         output_file="LeClosDesMimosas.png",
     )
+
+    image_to_pdf(image_path=qrcode_image, size=12)
